03966be3044d4ce9f60bad6f3a1eed34a60e1cb7
Keekee <keenan.guiet@gmail.com>
2024-02-16 22:37:48 +0100

    commit

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -42,5 +42,33 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.patch('/:id', async (req, res) => {
+    try {
+        const expense = await Expense.findByIdAndUpdate(req.params.id, {
+            expenseAmount,
+            expenseCategory,
+            expenseDate,
+            expenseDescription
+        }, { new: true })
+        if (!expense) {
+            res.status(404).json({ message: 'Ah fu' })
+        }
+        res.json(expense)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedExpense = await Expense.findByIdAndDelete(req.params.id)
+        if (!deletedExpense) {
+            res.status(404).json({ message: 'ah super fuh' })
+        }
+        res.json(deletedExpense)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 module.exports = router;
